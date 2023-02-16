142cbcc11d0036275cedef540531a155dbaaeb37
ferranjrr <ferrandelavargaantoja@gmail.com>
2023-02-16 18:32:51 +0000

    Add second screen sport + buttons

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     TextView OurText;
     Button TheButton;
 
@@ -21,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
         //setting the variables to the Layout file
         OurText = findViewById(R.id.textView);
         TheButton = findViewById(R.id.button);
+
+        Button TennisButton = findViewById(R.id.Tennis);
+
+        TennisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Sport.class));
+            }
+        });
 
         //Setting the OnClick Listener
         TheButton.setOnClickListener(new View.OnClickListener() {
