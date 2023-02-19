637875ddb9ea5172a0ed7f9d2ba9f29626b1bb59
ferranjrr <ferrandelavargaantoja@gmail.com>
2023-02-19 14:55:05 +0000

    Request button for userA

--- app/src/main/java/com/example/myapplication/Sport.java
+++ app/src/main/java/com/example/myapplication/Sport.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Sport extends Activity {
 
@@ -23,5 +27,21 @@ public class Sport extends Activity {
 
         // display the string into textView
         SportNameLabel.setText(" " + str);
+
+
+        Button Request = findViewById(R.id.RequestUA);
+        Request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Request sent!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+                Request.setText("Amend Request");
+            }
+        });
     }
 }
